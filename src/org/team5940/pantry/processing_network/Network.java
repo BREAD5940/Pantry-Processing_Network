af6eaf4025218aa03ce68fabb11409f71b45faf0
David Boles <david.e.boles22@gmail.com>
2017-07-08 17:56:48 -0700

    Network code and javadoc improvements.

--- src/org/team5940/pantry/processing_network/Network.java
+++ src/org/team5940/pantry/processing_network/Network.java
@@ -6,19 +6,20 @@ import java.util.HashSet;
 /**
  * A Network is a thread that updates a set of {@link Node}s.
  * <br><br>
- * Timing on a network is done in cycles, code running within a cycle should not act differently because of time passing during the cycle.
- * Once a network is started, cycles occur every cycleDelay, some number of nanoseconds.
- * Due to technical limitations (specifically the amount of time it actually takes a network to complete a cycle and the accuracy of System.nanoTime())
+ * Timing on a network is done in cycles where internally events should appear to occur simultaneously
+ * 	(code running within a single cycle should not act differently because of time passing during that cycle).
+ * Once a network is started, cycles occur every cycleDelay, some number of microseconds (1/1,000,000 second).
+ * Due to technical limitations (specifically the amount of time it actually takes a network to complete a cycle and potentially the accuracy of System.nanoTime())
  * 	cycles may occur more slowly than specified but they will never occur more quickly. For timing needs inside of Nodes, {@link Network#getNetworkTime()} is available.
  * <br><br>
  * Nodes are added to a network automatically when they are initialized (as long as the network has not already been started).
- * Nodes must declare on initialization any {@link SourceNode}s they utilize which forces them to form a DAG (Directed Acyclic Graph) where no "looping" dependencies
- * 	are present.
- * This is necessary due to the "instantaneous" nature of network cycles, you cannot have operations that (indirectly) rely on themselves: the cycle would never
+ * Nodes must declare on initialization any {@link SourceNode}s they utilize which forces them to form a directed acyclic graph: no "looping" dependencies are present.
+ * This is necessary due to the "internally instantaneous" nature of network cycles, you cannot have operations that (indirectly) rely on themselves: the cycle would never
  * 	complete.
  * <br><br>
  * Nodes can perform some sort of operation up to once every cycle, when their doUpdate method is internally called.
  * A node is manually updated (if they haven't already been) by the network on a cycle if node.requiresUpdate() returns true.
+ * If updating the node returns an exception it is caught, the network continues to try to function.
  * Value nodes are also updated when another node calls node.getValue().
  * This system of updating values within cycles is advantageous because code using the values is guaranteed an up-to-date value (to within this cycle) but the value
  * 	can be buffered (not recalculated, saving computer resources) because it is guaranteed not to change.
@@ -29,7 +30,7 @@ import java.util.HashSet;
 public class Network extends Thread {
 
 	/**
-	 * The lowest number of nanoseconds between the start of cycles for this Network.
+	 * The lowest number of microseconds between the start of cycles for this Network.
 	 */
 	long cycleDelay;
 
@@ -44,12 +45,12 @@ public class Network extends Thread {
 	long currentCycle = -1;
 
 	/**
-	 * The value of System.nanoTime() when this is first started.
+	 * The value of System.nanoTime()/1000 when this is first started.
 	 */
 	long startTime;
 	
 	/**
-	 * The value of System.nanoTime() when the last (potentially current) cycle started.
+	 * The value of System.nanoTime()/1000 when the last (potentially currently executing) cycle started.
 	 */
 	long lastCycleStart;
 
@@ -57,93 +58,106 @@ public class Network extends Thread {
 	 * Creates a new Network with the given cycle delay.
 	 * 
 	 * @param cycleDelay
-	 *            The minimum number of nanoseconds between the beginning of cycles.
+	 *            The minimum number of microseconds between the beginning of cycles. Must be greater than or equal to 0.
+	 * @throws IllegalArgumentException cycleDelay is less than 0.
 	 */
-	public Network(int cycleDelay) {
+	public Network(long cycleDelay) throws IllegalArgumentException {
+		if(cycleDelay < 0) {
+			//TODO log
+			throw new IllegalArgumentException("Illegal Value For cycleDelay");
+		}
 		this.cycleDelay = cycleDelay;
 		this.nodes = new HashSet<>();
 	}
 
 	@Override
 	public void run() {
-		this.startTime = System.nanoTime();
-		this.currentCycle = 0;
+		this.startTime = microTime();
 		while (!this.isInterrupted()) {
-			this.lastCycleStart = System.nanoTime();
-//			long cycleStartTime = System.nanoTime();
-//			//this.currentCycle++;
-//			for (Node node : this.nodes) {
-//				if (node.requiresUpdate()) {
-//					try {//TODO add documentation about failed updates
-//						node.update();
-//					} catch (IllegalUpdateThreadException e) {
-//						//TODO logging stuff
-//					}
-//				}
-//			}
-//			long cycleEndTime = System.nanoTime();
-//			long extraTime = this.cycleDelay - (cycleEndTime - cycleStartTime) / 1000;
-//			if (extraTime > 0) {
-//				try {
-//					Thread.sleep(extraTime);
-//				} catch (InterruptedException e) {
-//					e.printStackTrace();
-//				}
-//			}
+			this.lastCycleStart = microTime();
+			this.currentCycle++;
+			for (Node node : this.nodes) {
+				if (node.requiresUpdate()) {
+					try {
+						node.update();
+					} catch (IllegalUpdateThreadException e) {
+						//TODO log
+					}
+				}
+			}
+			long cycleEndTime = microTime();
+			long extraTime = this.cycleDelay - (cycleEndTime - this.lastCycleStart);
+			if (extraTime > 0) {
+				try {
+					Thread.sleep(extraTime/1000, (int) ((extraTime-(extraTime/1000))*1000));
+				} catch (InterruptedException e) {
+					//TODO log
+				}
+			}
 			
 		}
 	}
 
 	/**
-	 * Adds a Node for this Network to update every cycle.
-	 * 
-	 * @param node
-	 *            The Node to add to this Network.
-	 * @throws IllegalStateException
-	 *             If the Network is already running.
+	 * Adds a Node for this Network.
+	 * @param node The Node to add to this Network.
+	 * @throws IllegalStateException the Network has already been started.
+	 * @throws IllegalArgumentException node is null or this isn't its network.
 	 */
-	protected void addNode(Node node) throws IllegalStateException {
-		if (this.isAlive()) {
-			throw new IllegalStateException("Network is already running");
+	protected synchronized void addNode(Node node) throws IllegalStateException, IllegalArgumentException {
+		if (this.getState() != Thread.State.NEW){
+			//TODO log
+			throw new IllegalStateException("Network Already Started");
 		}
-		if (node != null) {
-			// TODO change this to check that node has this as its Network. 
-			nodes.add(node);
+		if(node == null) {
+			//TODO log
+			throw new IllegalArgumentException("Null Node");
 		}
+		if(node.getNetwork() != this) {
+			//TODO log
+			throw new IllegalArgumentException("Wrong Node Network");
+		}
+		this.nodes.add(node);
 	}
 
 	/**
-	 * Gets the current cycle that this Network is on.
-	 * 
-	 * @return The current cycle this Network is on. If Network is not running
-	 *         returns -1.
+	 * Gets the cycle delay of the network.
+	 * @return The minimum number of microseconds between the start of cycles.
 	 */
-	public long getCurrentCycle() {
-		if (this.isAlive())
-			return currentCycle;
-		else
-			return -1;
-	}
-
-	/**
-	 * Gets the update rate of this Network in milliseconds. Is not necessarily
-	 * the update rate. It could go slower.
-	 * 
-	 * @return The update rate of the Network in milliseconds.
-	 */
-	public int getUpdateRate() {
+	public long getCycleDelay() {
 		return this.cycleDelay;
 	}
 
 	/**
-	 * Gets the length of time this Network has run in milliseconds.
-	 * 
-	 * @return The length of time this Network has run in milliseconds. If Network is not running returns -1. 
+	 * Gets the last cycle this network started (potentially a currently executing cycle).
+	 * @return The current cycle this Network is on. If Network is not running
+	 *         returns -1.
 	 */
-	public long getNetworkTime() {
-		if (this.isAlive())
-			return (System.nanoTime() - this.startTime) / 1000;
-		else 
+	public long getLastCycle() {
+		if (this.isAlive()) {
+			return currentCycle;
+		}else {
 			return -1;
+		}
+	}
+
+	/**
+	 * Gets the time in microseconds from when the network started to the start of the last cycle (potentially a currently executing cycle).
+	 * @return Microseconds since network start to last cycle start. Returns -1 if the network is not running. 
+	 */
+	public long getLastCycleStart() {
+		if (this.isAlive()) {
+			return this.lastCycleStart-this.startTime;
+		}else {
+			return -1;
+		}
+	}
+	
+	/**
+	 * Internal method for getting System.nanoTime() as microseconds.
+	 * @return System.nanoTime()/1000
+	 */
+	long microTime() {
+		return System.nanoTime()/1000;
 	}
 }
