8650325bb385f785cbf3bb28406c90544f44a99a
BertTurtle <mbentley124@gmail.com>
2017-07-12 17:19:17 -0700

    Fixed spacing for SourceNodeTesterObject

--- tests/org/team5940/pantry/processing_network/SourceNodeTesterObject.java
+++ tests/org/team5940/pantry/processing_network/SourceNodeTesterObject.java
@@ -1,20 +1,19 @@
-package org.team5940.pantry.processing_network; 
- 
-import org.team5940.pantry.processing_network.Network;
-import org.team5940.pantry.processing_network.SourceNode; 
- 
-public class SourceNodeTesterObject extends SourceNode<Integer> { 
- 
-  public SourceNodeTesterObject(Network network, boolean requireUpdate, SourceNode<?> source) 
-      throws IllegalArgumentException, IllegalStateException { 
-    super(network, requireUpdate, source); 
-  }
+package org.team5940.pantry.processing_network;
 
-  @Override
-  public Integer updateValue() {
-	  //For Testing Purposes
-	  int ret = 3;
-	  return ret;
-  } 
- 
-} 
+import org.team5940.pantry.processing_network.Network;
+import org.team5940.pantry.processing_network.SourceNode;
+
+public class SourceNodeTesterObject extends SourceNode<Integer> {
+
+	public SourceNodeTesterObject(Network network, boolean requireUpdate, SourceNode<?> source)
+			throws IllegalArgumentException, IllegalStateException {
+		super(network, requireUpdate, source);
+	}
+
+	@Override
+	public Integer updateValue() {
+		// For Testing Purposes
+		int ret = 3;
+		return ret;
+	}
+}
