f36acc74b4f3e0c4dd4a9fa43f3af2ed26e4443c
BertTurtle <mbentley124@gmail.com>
2017-07-17 16:45:44 -0700

    Removed unnecesary whitespace.

--- src/org/team5940/pantry/processing_network/data_flow/NodeUtils.java
+++ src/org/team5940/pantry/processing_network/data_flow/NodeUtils.java
@@ -6,8 +6,6 @@ import java.util.Arrays;
 public class NodeUtils {
 
 	public static <T extends Object> T[] appendValueToArray(T value, T... array) {
-		
-		
 		T[] newArray = Arrays.copyOf(array, array.length + 1);
 		newArray[array.length] = value;
 		
