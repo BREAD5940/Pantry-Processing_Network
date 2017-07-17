784f9c5370af0c95b6af2557d80ff2d1ad4d4a6a
BertTurtle <mbentley124@gmail.com>
2017-07-17 16:30:55 -0700

    Fixed setting the source nodes for MultiplexerValueNode

--- src/org/team5940/pantry/processing_network/data_flow/MultiplexerValueNode.java
+++ src/org/team5940/pantry/processing_network/data_flow/MultiplexerValueNode.java
@@ -7,17 +7,23 @@ import java.util.HashSet;
 import org.team5940.pantry.processing_network.Network;
 import org.team5940.pantry.processing_network.SourceNode;
 
+import com.sun.xml.internal.bind.v2.model.util.ArrayInfoUtil;
+
 public class MultiplexerValueNode<T, S> extends SourceNode<T> {
 
-	public MultiplexerValueNode(Network network, SourceNode<Enum<? extends S>> stateSource, SourceNode<? extends T>... valueSources)
-			throws IllegalArgumentException, IllegalStateException {
-		super(network, false, );
-		
+	public MultiplexerValueNode(Network network, SourceNode<Enum<? extends S>> stateSource,
+			SourceNode<? extends T>... valueSources) throws IllegalArgumentException, IllegalStateException {
+		super(network, new HashSet<SourceNode>() {
+			{
+				add(stateSource);
+				addAll(Arrays.asList(valueSources));
+			}
+		}.toArray(new SourceNode[valueSources.length + 1]));
 	}
 
 	@Override
 	protected T updateValue() {
 		return null;
 	}
-	
+
 }
