450ebdfa9abb809b2896cd8a4db03dd2f26eaac2
BertTurtle <mbentley124@gmail.com>
2017-07-17 16:44:40 -0700

    Created NodeUtils

---
+++ src/org/team5940/pantry/processing_network/data_flow/NodeUtils.java
@@ -0,0 +1,17 @@
+package org.team5940.pantry.processing_network.data_flow;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class NodeUtils {
+
+	public static <T extends Object> T[] appendValueToArray(T value, T... array) {
+		
+		
+		T[] newArray = Arrays.copyOf(array, array.length + 1);
+		newArray[array.length] = value;
+		
+		return newArray;
+	}
+
+}
