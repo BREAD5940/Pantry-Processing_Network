89eaef3acc15519cfb96ae2152bb0ad3af674763
BertTurtle <mbentley124@gmail.com>
2017-07-17 17:42:09 -0700

    Created ConstantValueNode.

---
+++ src/org/team5940/pantry/processing_network/data_flow/ConstantValueNode.java
@@ -0,0 +1,36 @@
+package org.team5940.pantry.processing_network.data_flow;
+
+import org.team5940.pantry.processing_network.Network;
+import org.team5940.pantry.processing_network.ValueNode;
+
+/**
+ * A node that returns a constant value of type T. 
+ * @author Michael Bentley
+ *
+ * @param <T> The type of value this node returns. 
+ */
+public class ConstantValueNode<T> extends ValueNode<T> {
+	
+	/**
+	 * The value this node always returns. 
+	 */
+	final T value;
+	
+	/**
+	 * Creates a Node which returns a constant value of type T. 
+	 * @param network The network this node belongs to. 
+	 * @param value The value this node always returns. 
+	 */
+	public ConstantValueNode(Network network, T value)
+			throws IllegalArgumentException, IllegalStateException {
+		super(network);
+		
+		this.value = value;
+	}
+
+	@Override
+	protected T updateValue() {
+		return this.value;
+	}
+
+}
