3852e5ac9255a14a3f7c2280b78b38a74f83b144
BertTurtle <mbentley124@gmail.com>
2017-07-24 17:34:21 -0700

    Added Tests for NodeGroup

---
+++ tests/org/team5940/pantry/processing_network/data_flow/NodeGroupTest.java
@@ -0,0 +1,75 @@
+package org.team5940.pantry.processing_network.data_flow;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+import org.team5940.pantry.processing_network.Network;
+import org.team5940.pantry.processing_network.Node;
+import org.team5940.pantry.processing_network.NodeTesterObject;
+import org.team5940.pantry.processing_network.ValueNode;
+import org.team5940.pantry.processing_network.ValueNodeTesterObject;
+
+public class NodeGroupTest {
+
+	@Test
+	public void testNodeGroup_Standard() {
+		Network network = new Network(2000);
+
+		ValueNode<?> valueNode1 = new ValueNodeTesterObject(network, true);
+		ValueNode<?> valueNode2 = new ValueNodeTesterObject(network, true);
+
+		ValueNode<?> valueNode = new ValueNodeTesterObject(network, true, valueNode1, valueNode2);
+		Node node = new NodeTesterObject(network, true, valueNode);
+
+		ValueNode<?> randomNode = new ValueNodeTesterObject(network, true);
+
+		NodeGroupTesterObject testGroup = new NodeGroupTesterObject(network, valueNode, randomNode, node);
+
+		Set<Node> outputNodeTest = new HashSet<>();
+		outputNodeTest.add(node);
+		outputNodeTest.add(randomNode);
+
+		assertEquals(outputNodeTest, testGroup.enumerateOutputs());
+
+		Set<ValueNode<?>> setSources = testGroup.enumerateSources();
+
+		assertEquals(true, setSources.contains(valueNode2));
+		assertEquals(true, setSources.contains(valueNode1));
+
+		network.start();
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void testNodeGroup_WrongNetwork_IllegalArgumentException() {
+		Network network = new Network(2000);
+		
+		Network wrongNetwork = new Network(2000);
+		
+		ValueNode<?> valueNode = new ValueNodeTesterObject(wrongNetwork, true);
+		
+		new NodeGroupTesterObject(network, valueNode);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testNodeGroup_NullNode_IllegalArgumentException() {
+		Network network = new Network(2000);
+				
+		ValueNode<?> valueNode = new ValueNodeTesterObject(network, true);
+		
+		new NodeGroupTesterObject(network, valueNode, null);
+	}
+	
+	@Test (expected = IllegalStateException.class)
+	public void testNodeGroup_NetworkStarted_IllegalStateException() {
+		Network network = new Network(2000);
+		
+		ValueNode<?> valueNode = new ValueNodeTesterObject(network, true);
+		
+		network.start();
+		
+		new NodeGroupTesterObject(network, valueNode, null);
+	}
+}
---
+++ tests/org/team5940/pantry/processing_network/data_flow/NodeGroupTesterObject.java
@@ -0,0 +1,15 @@
+package org.team5940.pantry.processing_network.data_flow;
+
+import org.team5940.pantry.processing_network.Network;
+import org.team5940.pantry.processing_network.Node;
+
+public class NodeGroupTesterObject extends NodeGroup {
+		
+	public NodeGroupTesterObject(Network network, Node... nodes) {
+		super(network);
+		
+		for (Node node : nodes) {
+			this.addNode(node);
+		}
+	}
+}
