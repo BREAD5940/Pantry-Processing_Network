10eeb922393d3c9a54f448823ecb6114a0485515
BertTurtle <mbentley124@gmail.com>
2018-01-27 15:21:18 -0800

    Improved ChangeDetectorNode

--- src/main/java/org/team5940/pantry/processing_network/functional/ChangeDetectorNode.java
+++ src/main/java/org/team5940/pantry/processing_network/functional/ChangeDetectorNode.java
@@ -1,6 +1,5 @@
 package org.team5940.pantry.processing_network.functional;
 
-import org.team5940.pantry.logging.LoggingUtils;
 import org.team5940.pantry.logging.loggers.Logger;
 import org.team5940.pantry.processing_network.Network;
 import org.team5940.pantry.processing_network.Node;
@@ -8,53 +7,49 @@ import org.team5940.pantry.processing_network.ValueNode;
 
 /**
  * This detects when the value of a ValueNode is changed. When the value is
- * changed this runs a {@link GenericLambda} with the new value as the argument.
- * This uses .equals to see if the value has changed.
+ * changed this runs valueChanged with the new value as the argument. This uses
+ * .equals to see if the value has changed.
+ * 
+ * IMPORTANT. This should not be used to edit variables but only other nodes.
+ * IMPORTANT
  * 
  * @author Michael Bentley
  *
  * @param <T>
  *            The type of value the ValueNode returns.
  */
-public class ChangeDetectorNode<T> extends Node {
+
+public abstract class ChangeDetectorNode<T> extends Node {
 
 	/**
-	 * The ValueNode to detect when the value switches. 
+	 * The ValueNode to check when the value has changed.
 	 */
 	ValueNode<T> source;
-	
-	/**
-	 * The previous value of the source. 
-	 */
-	T previousValue;
-	
-	/**
-	 * The GenericLambda to run with the new value when the value of source changes. 
-	 */
-	GenericLambda<T> newValueLambda;
 
 	/**
-	 * This detects when a value from a ValueNode changes. When it does it runs
-	 * newValueLambda with the new value as the argument.
+	 * The previous value of the ValueNode to compare the new value against.
+	 */
+	T previousValue;
+
+	/**
+	 * This detects when a ValueNode value changes. It runs valueChanged when it has
+	 * changed with the new value.
 	 * 
 	 * @param network
-	 *            This' Network.
+	 *            This' Network
 	 * @param logger
-	 *            This' Logger.
-	 * @param requiresUpdate
-	 *            Specifies if this node should be updated by the network.
+	 *            This' Logger
+	 *            @param label
+	 *            This' label.
+	 * @param requireUpdate
+	 *            If this should be updated.
 	 * @param source
-	 *            The ValueNode to check for changes.
-	 * @param newValueLambda
-	 *            The lambda to run if changes are found. The argument will be the
-	 *            new value of the source.
+	 *            The ValueNode to check if the value has changed.
 	 */
-	public ChangeDetectorNode(Network network, Logger logger, boolean requiresUpdate, ValueNode<T> source,
-			GenericLambda<T> newValueLambda) throws IllegalArgumentException, IllegalStateException {
-		super(network, logger, requiresUpdate, source);
-		LoggingUtils.checkArgument(newValueLambda);
+	public ChangeDetectorNode(Network network, Logger logger, String label, boolean requireUpdate, ValueNode<T> source)
+			throws IllegalArgumentException, IllegalStateException {
+		super(network, logger, label, requireUpdate, source);
 		this.source = source;
-		this.newValueLambda = newValueLambda;
 	}
 
 	@Override
@@ -63,8 +58,15 @@ public class ChangeDetectorNode<T> extends Node {
 			this.previousValue = this.source.getValue();
 		} else if (!this.previousValue.equals(this.source.getValue())) {
 			this.previousValue = this.source.getValue();
-			this.newValueLambda.lambda(this.source.getValue());
+			this.valueChanged(this.source.getValue());
 		}
 	}
 
+	/**
+	 * Run when the value of the ValueNode has changed.
+	 * 
+	 * @param newValue
+	 *            The new value of the ValueNode.
+	 */
+	protected abstract void valueChanged(T newValue);
 }
