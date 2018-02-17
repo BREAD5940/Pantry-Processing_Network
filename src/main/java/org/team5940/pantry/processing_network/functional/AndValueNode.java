588240ce86f728df23ca0d50a70faa287145b8d7
pdowd21 <35285212+pdowd21@users.noreply.github.com>
2018-02-17 15:07:19 -0800

    created AndValueNode and LatchingBooleanValueNode (#6)
    * created AndValueNode and LatchingBooleanValueNode
    
    * Removed == true from if statement

---
+++ src/main/java/org/team5940/pantry/processing_network/functional/AndValueNode.java
@@ -0,0 +1,70 @@
+package org.team5940.pantry.processing_network.functional;
+
+import org.team5940.pantry.logging.loggers.Logger;
+import org.team5940.pantry.processing_network.Network;
+import org.team5940.pantry.processing_network.ValueNode;
+
+/**
+ * Checks to see whether two values are both true
+ * 
+ * @author Paul Dowd
+ *
+ */
+
+public class AndValueNode extends ValueNode<Boolean> {
+
+	/**
+	 * Creates the first value being checked
+	 */
+
+	ValueNode<? extends Boolean> valueNode1;
+
+	/**
+	 * Creates the second value being checked
+	 */
+
+	ValueNode<? extends Boolean> valueNode2;
+
+	/**
+	 * creates the other value being checked and the boolean to return
+	 */
+
+	Boolean bothBooleansTrue = false;
+
+	/**
+	 * creates a new AndNode to check whether two values are both true
+	 * 
+	 * @param network
+	 *            this' network
+	 * @param logger
+	 *            this' logger
+	 * @param label
+	 *            this' label
+	 * @param valueNode1
+	 *            the first ValueNode being checked
+	 * @param valueNode2
+	 *            the second ValueNode being checked
+	 */
+
+	public AndValueNode(Network network, Logger logger, String label, ValueNode<? extends Boolean> valueNode1,
+			ValueNode<? extends Boolean> valueNode2) throws IllegalArgumentException, IllegalStateException {
+		super(network, logger, label, valueNode1, valueNode2);
+		this.valueNode1 = valueNode1;
+		this.valueNode2 = valueNode2;
+	}
+
+	@Override
+	protected Boolean updateValue() {
+		/**
+		 * checks the variables and if both are true, changes the return boolean and
+		 * returns it
+		 */
+
+		if (this.valueNode1.getValue() && valueNode2.getValue()) {
+			bothBooleansTrue = true;
+		}
+
+		return bothBooleansTrue;
+	}
+
+}
---
+++ src/main/java/org/team5940/pantry/processing_network/functional/LatchingBooleanValueNode.java
@@ -0,0 +1,52 @@
+package org.team5940.pantry.processing_network.functional;
+
+import org.team5940.pantry.logging.loggers.Logger;
+import org.team5940.pantry.processing_network.Network;
+import org.team5940.pantry.processing_network.ValueNode;
+
+/**
+ * Changes a ValueNode to a normal boolean for use when remembering 
+ * that something has happened when another variable or object would
+ * revert back to a different state
+ * 
+ * @author Paul Dowd
+ *
+ */
+
+public class LatchingBooleanValueNode extends ValueNode<Boolean> {
+	
+	/**
+	 * The constant boolean to convert to
+	 */
+	Boolean isTrue = false;
+	
+	/**
+	 * The ValueNode converted from
+	 */
+	ValueNode<? extends Boolean> valueNode;
+	
+	/**
+	 * makes the ValueNode constant if the ValueNode is true
+	 * 
+	 * @param network this' network
+	 * @param logger this' logger
+	 * @param label this' label
+	 * @param valueNode the ValueNode which the state is taken from
+	 */
+	
+	public LatchingBooleanValueNode(Network network, Logger logger, String label,
+			ValueNode<? extends Boolean> valueNode)
+			throws IllegalArgumentException, IllegalStateException {
+		super(network, logger, label, valueNode);
+
+		this.valueNode = valueNode;
+	}
+
+	@Override
+	protected Boolean updateValue() {
+		if (valueNode.getValue() == true) {
+			isTrue = true;
+		}
+		return isTrue;
+	}
+}
