9bdf4d6d53bbef0809f8557dd16342f1a025bc8e
BertTurtle <mbentley124@gmail.com>
2018-03-27 19:59:09 -0700

    Created a CircleValueNode that can be used to make circles in a Network
    This might not be done in the best way. Should verify with others that this is a good way to do it.

--- src/test/java/org/team5940/pantry/processing_network/CircleValueNode.java
+++ src/test/java/org/team5940/pantry/processing_network/CircleValueNode.java
@@ -4,14 +4,21 @@ import org.team5940.pantry.logging.loggers.Logger;
 
 public class CircleValueNode<T> extends ValueNode<T> {
 
-	ValueNode<T> setValueNode;
+	ValueNode<? extends T> setValueNode;
+	T intialValue;
+	boolean firstUpdate = true;
 
-	public CircleValueNode(Network network, Logger logger, String label)
+	public CircleValueNode(Network network, Logger logger, String label, T intialValue)
 			throws IllegalArgumentException, IllegalStateException {
 		super(network, logger, label);
+		this.intialValue = intialValue;
 	}
 
-	public void setGetValue(ValueNode<T> setValueNode) {
+	public void setGetValue(ValueNode<? extends T> setValueNode) {
+		System.out.println("Set Get Value: " + setValueNode.getLabel().toString());
+		if (setValueNode == this) {
+			this.getLogger().throwError(this, new IllegalArgumentException("Cannot set setValueNode to itself"));
+		}
 		this.setValueNode = setValueNode;
 	}
 
@@ -20,6 +27,14 @@ public class CircleValueNode<T> extends ValueNode<T> {
 		if (this.setValueNode == null) {
 			this.getLogger().throwError(this, new IllegalStateException("Network started without setValueNode set"));
 		}
+		if (firstUpdate) {
+			firstUpdate = false;
+			return intialValue;
+		}
+		// TODO this is sketchy. Should replace.
+		this.lastUpdate = this.getNetwork().getLastCycle();
+		System.out.println("Circle Value: " + this.setValueNode.getValue());
+		// System.out.println("Set Value Value: " + this.setValueNode.getValue());
 		return this.setValueNode.getValue();
 	}
 
